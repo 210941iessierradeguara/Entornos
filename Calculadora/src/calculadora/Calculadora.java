ced50fc5422a0703dcac448c6236c3e0c46c1b00
alumno <210941@iessierradeguara.com>
2022-05-27 12:30:25 +0200

    2022-05-27

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -4,15 +4,32 @@ public class Calculadora {
 	int num1;
 	int num2;
 	
+	/**
+	 * 
+	 * @param num1 Número entero
+	 * @param num2 Número entero
+	 */
 	public Calculadora(int num1, int num2) {
 		super();
 		this.num1 = num1;
 		this.num2 = num2;
 	}
 	
+	/**
+	 * 
+	 * @return Devuelve la suma de los números num1 y num2 que son atributos del objeto. 
+	 */
+	
 	public int suma() {
 		return num1+num2;
 	}
+	
+	/**
+	 * 
+	 * @return la suma de los números con un segundo de retardo
+	 * @throws InterruptedException lanzado por el metodo sleep de la clase thread
+	 * {@code Thread.sleep(300);}
+	 */
 	
 	public int sumaLenta(){
 		try {
